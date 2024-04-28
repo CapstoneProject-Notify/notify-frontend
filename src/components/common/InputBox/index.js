d9b9dc7803a5d144bbbe68fa5617b4213f621017
임영 <133564032+loving5917@users.noreply.github.com>
2024-04-29 05:58:33 +0900

    Revert ""feat. Box, Button 생성""

---
+++ src/components/common/InputBox/index.js
@@ -0,0 +1,10 @@
+import { BoxBackground } from "./styles";
+
+function InputBox() {
+  return (
+    <BoxBackground>
+    </BoxBackground>
+  );
+}
+
+export default InputBox;
---
+++ src/components/common/InputBox/styles.js
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const BoxBackground = styled.input`
+  background-color: ${({ theme }) => theme.color.gray};
+  height: 46px;
+  width: 700px;
+  border: none;
+  border-radius: 10px;
+`;
