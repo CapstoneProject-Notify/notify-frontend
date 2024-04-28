c9846179c3eaf28969590e5dc92a4aa1d2fc4a3e
linying <loving5917@gmail.com>
2024-04-29 05:48:46 +0900

    Revert "feat. Box, Button 생성"
    This reverts commit a3f1719653b8e2ae8a557bd9de51d9f659e3bf93.

--- src/components/common/WithdrawalButton/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from "@emotion/styled";
-
-export const OrangeBackground = styled.button`
-  background-color: ${({ theme }) => theme.color.orange};
-  color: #FFFFFF;
-  border-radius: 5px;
-  height: 46px;
-  width: 140px;
-`;
