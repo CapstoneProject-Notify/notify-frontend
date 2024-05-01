7ae22772046aff29a557bb5326389e2609f66594
YerinKim929 <nirey929@gmail.com>
2024-05-01 12:57:14 +0900

    fix: test.json 변경

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -5,41 +5,11 @@ import Title from "../../components/common/Title";
 import styled from "@emotion/styled/macro";
 import { commonAxios } from "../../utils/commonAxios";
 import { useEffect, useState } from "react";
-
-const data = {
-  code: 200,
-  message: "공지사항 조회 성공",
-  data: {
-    page: 1,
-    totalPages: 41,
-    notices: [
-      {
-        noticeId: 1,
-        title: "장학금안내",
-        noticeDate: "2024-02-02",
-        isScrapped: false,
-        url: "www.aaa.com",
-      },
-      {
-        noticeId: 1,
-        title: "장학금",
-        noticeDate: "2024-02-02",
-        isScrapped: false,
-        url: "www.aaa.com",
-      },
-      {
-        noticeId: 1,
-        title: "장학",
-        noticeDate: "2024-02-02",
-        isScrapped: false,
-        url: "www.aaa.com",
-      },
-    ],
-  },
-};
+import TestData from "../../constants/test.json";
 
 function MainPage() {
   const [newPost, setNewPost] = useState([]);
+  const data = TestData;
 
   const getPost = () => {
     commonAxios
