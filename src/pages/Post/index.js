68c276476e50212e7ba145ddc3c4d11453f9c894
YerinKim929 <nirey929@gmail.com>
2024-06-08 01:29:38 +0900

    fix: category data 수정

--- src/pages/Post/index.js
+++ src/pages/Post/index.js
@@ -9,7 +9,67 @@ import { useEffect, useState } from "react";
 import NoticeList from "../../components/NoticeList";
 import { useLocation } from "react-router-dom";
 import data from "../../constants/test.json";
+import categoryData from "../../constants/category.json";
 import Pagination from "../../components/Pagination";
+
+const com = [
+  "전체",
+  "입학",
+  "취업",
+  "채용/모집",
+  "장학",
+  "행사/세미나",
+  "일반",
+  "기타",
+];
+const bus = [
+  "전체",
+  "학사",
+  "입학",
+  "취업",
+  "채용/모집",
+  "장학",
+  "행사/세미나",
+  "일반",
+  "학생회",
+  "소식",
+  "기타",
+];
+const cos = [
+  "전체",
+  "학사",
+  "수강신청",
+  "입학",
+  "취업",
+  "채용/모집",
+  "장학",
+  "행사/세미나",
+  "일반",
+  "기타",
+];
+const aai = [
+  "전체",
+  "학사",
+  "수강신청",
+  "입학",
+  "취업",
+  "채용/모집",
+  "장학",
+  "행사/세미나",
+  "일반",
+  "기타",
+];
+const esm = [
+  "전체",
+  "학사",
+  "졸업",
+  "ABEEK",
+  "현장실습",
+  "장학",
+  "채용/모집",
+  "공모전/세미나",
+  "기타",
+];
 
 function PostPage() {
   const [postInfo, setPostInfo] = useState(data.data);
@@ -23,11 +83,20 @@ function PostPage() {
   const type = typeUrl == "?type=com" ? "COM" : major;
   const [scrap, setScrap] = useState(false);
 
+  // 이번에 추가한 부분
+  const [category, setCategory] = useState("all");
+  const [search, setSearch] = useState("");
+
+  // get 부분의 url과 useEffect 부분에 두 변수 넣어서 바뀌면 재렌더되도록 수정했습니다!!
+
   const getNotice = () => {
     commonAxios
-      .get(`/notice${typeUrl}&page=${page}`, {
-        headers: { googleId: memId },
-      })
+      .get(
+        `/notice${typeUrl}&page=${page}&category=${category}&search=${search}`,
+        {
+          headers: { googleId: memId },
+        }
+      )
       .then((res) => {
         setPostInfo(res.data.data);
         console.log(res);
@@ -39,7 +108,7 @@ function PostPage() {
 
   useEffect(() => {
     getNotice();
-  }, [page, scrap, typeUrl]);
+  }, [page, scrap, typeUrl, category, search]);
 
   return (
     <>
